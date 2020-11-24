3d7500248766eb9eb0a278903202bb5eacdd3883
abedinforhane <farhanengn@gmail.comm>
2020-11-24 03:45:37 -0800

    navbar added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyNavbar from './Components/Navbar/MyNavbar';
+
+function App() {
+  return (
+    <div className="app">
+      <MyNavbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/MyNavbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import styles from './MyNavbar.module.css';
+
+const MyNavbar = () => {
+    return (
+     <>
+          <Navbar collapseOnSelect 
+          expand="md"  
+          variant="dark"
+          className={[styles.nav__theme , styles.animate__navbar , 'justify-content-between'].join(' ')}
+          >
+            <Navbar.Brand href="#home">
+                <h3 className={styles.nav__logo}>MezbauL AbediN ForhaN</h3>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#about">About</Nav.Link>
+                <Nav.Link href="#skilss">Skilss</Nav.Link>
+                <Nav.Link href="#experiences">Experiences</Nav.Link>
+                <Nav.Link href="#projects">Projects</Nav.Link>
+                <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+      </Navbar>
+     </>
+    );
+};
+
+export default MyNavbar;
